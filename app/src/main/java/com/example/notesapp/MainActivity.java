22eebe913f671571da5f09f0da3ef6eabf3a64e9
AndrewSkv <skvoznikovav@yandex.ru>
2021-06-18 16:00:44 +0400

    кнопка и фрагмент корректировки даты

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.res.Configuration;
@@ -24,20 +25,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ListNotesFragment listNotesFragment = ListNotesFragment.newInstance(notes);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(inContainer, listNotesFragment)
-                .commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(inContainer, listNotesFragment);
+        fragmentTransaction.commit();
     }
 
     private void initNotes() {
         notes = new Notes();
-        notes.add(new Note("Заметка 1","Описание заметки 1","01.06.21 15:00"));
-        notes.add(new Note("Заметка 2","Описание заметки 2","31.05.21 14:15"));
-        notes.add(new Note("Заметка 3","Описание заметки 3","25.05.21 14:00"));
-        notes.add(new Note("Заметка 4","Описание заметки 4","15.06.21 13:00"));
-        notes.add(new Note("Заметка 5","Описание заметки 5","20.06.21 14:00"));
-        notes.add(new Note("Заметка 6","Описание заметки 6","15.06.21 15:00"));
-        notes.add(new Note("Заметка 7","Описание заметки 7","01.06.21 15:30"));
+        notes.add(new Note("Заметка 1","Описание заметки 1","01.06.2020"));
+        notes.add(new Note("Заметка 2","Описание заметки 2","31.05.2018"));
+        notes.add(new Note("Заметка 3","Описание заметки 3","25.05.2017"));
+        notes.add(new Note("Заметка 4","Описание заметки 4","17.04.2021"));
+        notes.add(new Note("Заметка 5","Описание заметки 5","20.06.2021"));
+        notes.add(new Note("Заметка 6","Описание заметки 6","15.06.2016"));
+        notes.add(new Note("Заметка 7","Описание заметки 7","10.06.2021"));
     }
 }
--- app/src/main/java/com/example/notesapp/NoteFragment.java
+++ app/src/main/java/com/example/notesapp/NoteFragment.java
@@ -1,5 +1,6 @@
 package com.example.notesapp;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +12,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 
 public class NoteFragment extends Fragment {
     private static final String KEY_NOTE = "KEY_NOTE";
+
+    Note note;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +31,9 @@ public class NoteFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (getArguments() != null) {
+            note = (Note) getArguments().getSerializable(KEY_NOTE);
+        }
     }
 
     @Override
@@ -34,11 +41,24 @@ public class NoteFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         if (getArguments() != null) {
-            Note note = (Note) getArguments().getSerializable(KEY_NOTE);
-
-            ((TextView)view.findViewById(R.id.date_time_note)).setText(note.getDateCreate());
+            ((TextView)view.findViewById(R.id.date_note)).setText(note.getDateCreate());
             ((TextView)view.findViewById(R.id.header_note)).setText(note.getName());
             ((TextView)view.findViewById(R.id.description_note)).setText(note.getDescription());
+
+            view.findViewById(R.id.btn_change_date_note).setOnClickListener(v -> {
+                ChangeDateNoteFragment changeDateNoteFragment = ChangeDateNoteFragment.newInstance(note.getDateCreate());
+                FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+
+                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    fragmentTransaction.replace(R.id.maincontainer,changeDateNoteFragment);
+                    fragmentTransaction.addToBackStack(null);
+                }else {
+                    fragmentTransaction.replace(R.id.note_container,changeDateNoteFragment);
+                    fragmentTransaction.addToBackStack(null);
+                }
+
+                fragmentTransaction.commit();
+            });
         }
     }
 
